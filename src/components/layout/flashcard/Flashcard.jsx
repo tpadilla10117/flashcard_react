71c55784fbc489c1476540b6d26ce7b829bc0612
Trinidad Padilla <tpadilla101@live.com>
2022-01-20 15:35:09 -0800

    got some basic styling

--- src/components/layout/flashcard/Flashcard.jsx
+++ src/components/layout/flashcard/Flashcard.jsx
@@ -1,12 +1,27 @@
 import React, {useState} from 'react';
+import './Flashcard.scss';
 
 function Flashcard( {flashcard} ) {
 /* State to manage card flipping: */
     const [cardflip, setCardFlip] = useState(false);
 
   return (
-    <div onClick={() => setCardFlip(!cardflip)}>
-        {cardflip ? flashcard.answer : flashcard.question}
+    <div className={`card ${cardflip ? 'flip' : ''}`}  onClick={() => setCardFlip(!cardflip)}>
+
+
+        <div className='frontside'>
+            {flashcard.question}
+            <div className='flashcard-options'>
+                {flashcard.options.map(option => {
+                    return <div className='flashcard-option'>{option}</div>
+                })}
+            </div>
+        </div>
+
+        <div className='backside'>
+            {flashcard.answer}
+        </div>
+
     </div>
   )
 }
